a9202b008252a888b5b7aab9eb56ed2abfba3bbf
Thor Kampefner <thorck@pm.me>
2024-02-03 19:23:34 -0800

    init

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+#![allow(unreachable_code)]
+#![allow(non_snake_case)]
+#![allow(clippy::clone_on_copy)]
+
+use error::MyError;
+
+mod error;
+#[cfg(test)] mod tests;
+mod utils;
+
+use tracing::info;
+
+fn main() -> Result<(), MyError> {
+  utils::setup()?;
+  info!("hello thor");
+
+  Ok(())
+}
